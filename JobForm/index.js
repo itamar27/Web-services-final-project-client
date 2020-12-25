87f83e55ddd132e21b6d4d24d558a97ab8a518a0
Itamar Yarden <itamary9@gmail.com>
2020-12-25 14:50:20 +0200

    updated 25/12

---
+++ JobForm/index.js
@@ -0,0 +1,25 @@
+$(document).ready(()=>{
+  numOfClicks = 1;
+  console.log(numOfClicks);
+
+  $("#add-goal").click(function () {
+
+    numOfClicks++;
+    if (numOfClicks <= 2) {
+  
+      let clone = original.cloneNode(true);
+      clone.childNodes[9].childNodes[1].value = "";
+      clone.childNodes[3].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_name";
+      clone.childNodes[5].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_sets";
+      clone.childNodes[7].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_reps";
+      clone.childNodes[9].childNodes[1].name = "ex_" + (numOfClicks + 1) + "_desc";
+      clone.id = "duplicator" + ((numOfClicks++)+1);
+      var txt = '<h6>Ex' + (numOfClicks ) +  '</h6>';
+      clone.childNodes[1].innerHTML = txt;
+      clones.push(clone);
+      original.parentNode.appendChild(clone);
+    }
+  })
+})
+
+
